a8b06348f2c4c3f17abfbeeec0342cf7a2f7df37
Doro Soromou <dsoromou@mltds.mlt.ca>
2022-12-27 00:03:08 -0400

    Removed unused import in index.js

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const Loc = require('./models/Localisation');
-const Role = require('./models/Role');
-const Emp = require('./models/Employee');
-const Dept = require('./models/Departement');
-const Users = require('./models/Users');
-const bcrypt = require('bcrypt');
-
 require('dotenv').config();
+const expressWinston = require('express-winston');
+const logger = require('./logger/logger');
+const bodyParser = require('body-parser');
 
 const usersRoutes = require("./routes/users");
 const roleRoutes = require("./routes/role");
@@ -16,11 +12,12 @@ const localisationRoutes = require("./routes/localisations");
 const employeeRoutes = require("./routes/employees");
 const departmentRoutes = require("./routes/departments");
 
+const api = process.env.API_URL;
 
-const { transports, format, level } = require('winston');
-const expressWinston = require('express-winston');
-const logger = require('./logger/logger');
-const bodyParser = require('body-parser');
+app.use(`${api}/employee`, employeeRoutes);
+app.use(`${api}/role`, roleRoutes);
+app.use(`${api}/localisation`, localisationRoutes);
+app.use(`${api}/users`, usersRoutes);
 
 //Middlewares
 app.use(bodyParser.json());
@@ -28,24 +25,10 @@ app.use(express.json());
 app.use(expressWinston.logger({
     winstonInstance: logger,
     statusLevels: true
- }))
-
-
-const api = process.env.API_URL;
+}))
 
 mongoose.connect(process.env.CONNECTION_STRING, { useNewUrlParser: true }, () => console.log('Connected'));
-
-
-
-app.use(`${api}/employee`, employeeRoutes);
-app.use(`${api}/role`, roleRoutes);
-app.use(`${api}/localisation`, localisationRoutes);
-app.use(`${api}/users`, usersRoutes);
-
-
 
 app.listen(3000, () => {
     console.log('Le serveur a demarré')
 })
-
-//mongoose.disconnect();
