7febf331c0f2b7c3cb76917c2f4e564735a805c7
Mohac-tech <mohac5662@gmail.com>
2022-12-26 23:25:36 -0400

    Joi validation and python scrypt to create data base

--- DB_Script/create_database.py
+++ DB_Script/create_database.py
@@ -9,28 +9,28 @@ import json
 
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 
-fiName = str(sys.argv[1])
-dbName = str(sys.argv[2])
-colName = str(sys.argv[3])
+def createdb(colName, fiName):
+    db = myclient.WorkSavy_DB
+    collection = db[colName]
 
-db = myclient[dbName]
-collection = db[colName]
+    with open(fiName) as file:
+      file_data = json.load(file)
 
-if db is not None:
-     if collection is not None:
-        pass
-     else: 
-        collection = db[colName]
-else:
-     db = myclient[dbName]
-     collection = db[colName]
+    for i in range(len(file_data)):
+      del file_data[i]['_id']
+    print(file_data)
+    collection.insert_many(file_data)
 
-with open(fiName) as file:
-    file_data = json.load(file)
+createdb("department", "depts.json")
+createdb("loca", "localisations.json")
+createdb("rol", "roles.json")
 
-for i in range(len(file_data)):
-     del file_data[i]['_id']
-print(file_data)
-collection.insert_many(file_data)
-
+#if db is not None:
+#     if collection is not None:
+#        pass
+#     else: 
+#        collection = db[colName]
+#else:
+#     db = myclient[dbName]
+#     collection = db[colName]
 
